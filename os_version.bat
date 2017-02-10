15b12020d61259621f30d82406069da66f734175
Ernest Dotson <edotson@gmail.com>
2017-02-10 17:52:27 -0500

    Refactor os_version.bat

--- os_version.bat
+++ os_version.bat
@@ -1,35 +1,61 @@
 @echo off
 rem **************************************************************************
-rem Purpose:     Check for OS Version
-rem Parameters:  N/A
-rem Returns:     N/A
-rem Notes:
+rem Purpose:    Check for OS Version
+rem Parameters: N/A
+rem Returns:    N/A
+rem Notes:      Sets environment variables: OS_VER_MAJOR, OS_VER_MINOR,
+rem             OS_VER_BUILD
 rem             Sets OS_VERSION environment variable to the major.minor OS
 rem             version
 rem             Sets OS_NAME environment variable to the name of the OS
 rem             version
-rem References: 
+rem References:
 rem             See http://msdn.microsoft.com/en-us/library/ms724832%28VS.85%29.aspx
 rem             See http://stackoverflow.com/questions/13212033/get-windows-version-in-a-batch-file
+rem             See http://ss64.com/nt/ver.html
+rem             See https://msdn.microsoft.com/en-us/library/windows/desktop/ms724832(v=vs.85).aspx
 rem **************************************************************************
 
 set OS_VERSION=
+set OS_VER_MAJOR=
+set OS_VER_MINOR=
+set OS_VER_BUILD=
 set OS_NAME=
 
-for /f "tokens=4-5 delims=. " %%i in ('ver') do set OS_VERSION=%%i.%%j
-if "%OS_VERSION%" == "10.0" set OS_NAME=Windows 10
-if "%OS_VERSION%" == "6.3"  set OS_NAME=Windows 8.1
-if "%OS_VERSION%" == "6.2"  set OS_NAME=Windows 8
-if "%OS_VERSION%" == "6.1"  set OS_NAME=Windows 7
-if "%OS_VERSION%" == "6.0"  set OS_NAME=Windows Vista
-if "%OS_VERSION%" == "5.2"  set OS_NAME=Windows XP Professional x64
-if "%OS_VERSION%" == "5.1"  set OS_NAME=Windows XP
-if "%OS_VERSION%" == "5.0"  set OS_NAME=Windows 2000
-if "%OS_VERSION%" == "4.90" set OS_NAME=Windows ME
-if "%OS_VERSION%" == "4.10" set OS_NAME=Windows 98
-if "%OS_VERSION%" == "4.00" set OS_NAME=Windows 95
-if "%OS_VERSION%" == "4.0"  set OS_NAME=Windows NT 4.0
+for /f "tokens=4,5,6 delims=[]. " %%G in ('ver') Do (set OS_VER_MAJOR=%%G& set OS_VER_MINOR=%%H& set OS_VER_BUILD=%%I) 
 
+goto sub%OS_VER_MAJOR%%OS_VER_MINOR%
+ 
+:sub51
+:sub52
+set OS_NAME=Windows XP/Windows Server 2003
+goto next
+ 
+:sub60
+set OS_NAME=Windows Vista/Windows Server 2008
+goto next
+ 
+:sub61
+set OS_NAME=Windows 7/Windows Server 2008 R2
+goto next
+ 
+:sub62
+set OS_NAME=Windows 8/Windows Server 2012
+goto next
+ 
+:sub63
+set OS_NAME=Windows 8.1/Windows Server 2012 R2
+goto next
+ 
+:sub100
+:sub101
+set OS_NAME=Windows 10/Windows Server 2016
+ 
+:next
+set OS_VERSION=%OS_VER_MAJOR%.%OS_VER_MINOR%
 
-echo OS_VERSION=%OS_VERSION%
-echo OS_NAME=%OS_NAME%
+rem echo OS_VERSION=%OS_VERSION%
+rem echo OS_VER_MAJOR=%OS_VER_MAJOR%
+rem echo OS_VER_MINOR=%OS_VER_MINOR%
+rem echo OS_VER_BUILD=%OS_VER_BUILD%
+rem echo OS_NAME=%OS_NAME%
